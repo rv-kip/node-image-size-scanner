2232dbee169384a8e2c7972574644d267abaec51
Kip Gebhardt <kgebhardt@kqed.org>
2014-10-31 17:33:09 -0700

    Inital commit

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-node-image-size-checker
+node-image-size-scanner
 =======================
+Reports the image file sizes for a given URL and optionally a minimum file size.
 
-Report the image file sizes for a given URL 
+## Installation ##
+`npm install node-image-size-scanner`
+
+## Usage ##
+```
+$ node check
+Usage: node check URL [max bytes allowed]
+Ex: node check http://www.google.com 50k
+
+$ node check www.google.com 1k
+Image files > 1.00 kB (1000 bytes)
+    1.83 kB http://www.google.com/images/icons/product/chrome-48.png
+  209.03 kB http://www.google.com/logos/doodles/2014/halloween-2014-5647569745084416.3-hp.gif
---
+++ check.js
@@ -0,0 +1,56 @@
+// Get image sizes for images on a given url
+var Scraper = require('image-scraper'),
+    request = require('request'),
+    Filesize = require('filesize'),
+    colors = require('colors/safe'),
+    sprintf=require("sprintf-js").sprintf;
+
+if (process.argv.length < 3) {
+    console.log("Usage: node check URL [max bytes allowed]");
+    console.log("Ex: " + colors.grey("node check http://www.google.com 50k"));
+    process.exit(1);
+}
+
+var test_url = process.argv[2],
+    max_size_bytes = process.argv[3] || 0;
+
+if (typeof(max_size_bytes) === "string" && max_size_bytes.match(/k/i)){
+    max_size_bytes = max_size_bytes.replace(/k/i, "");
+    max_size_bytes = +max_size_bytes * 1000;
+}
+
+if (max_size_bytes) {
+    console.log(colors.bold("Image files >" + Filesize(max_size_bytes) + " (" + max_size_bytes + " bytes)"));
+}
+
+var scraper = new Scraper (test_url);
+
+scraper.on("image", function(image){
+    processImage(image);
+});
+
+function processImage(image) {
+    var image_url = image.address;
+    request.head(image_url, function(err, res){
+        if (err) {
+            return console.error(colors.red("Error"), err);
+        }
+
+        if (res && res.headers['content-length']){
+            var file_size_bytes = +(res.headers['content-length']),
+                file_size = Filesize(file_size_bytes);
+
+            if (file_size_bytes > max_size_bytes) {
+                var formatted_file_size = sprintf("%11s", file_size);
+                // Images 3x the max size get highlighted in red
+                var formatted_output = colors.yellow(formatted_file_size);
+                if (file_size_bytes > (3 * max_size_bytes)) {
+                    formatted_output = colors.red(formatted_file_size);
+                }
+                formatted_output += " " + colors.cyan(image_url);
+                console.log(formatted_output);
+            }
+        }
+    });
+}
+scraper.scrape();
