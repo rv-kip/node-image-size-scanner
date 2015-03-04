32ac2cf51897ab9e4c41fafbf2ffc74535d3c45b
Kip Gebhardt <kgebhardt23@gmail.com>
2015-03-03 20:38:29 -0800

    JSON output format. Sorting by size

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 node-image-size-scanner
 =======================
-For a given URL, report image file sizes and paths with optional minimum file size to report on.
+For a given URL, report image file sizes and paths. Optionally you may specify a minimum file size to report on. Formatted output or json output.
 
 ## Installation ##
 `npm install -g node-image-size-scanner`
@@ -8,11 +8,24 @@ For a given URL, report image file sizes and paths with optional minimum file si
 ## Usage ##
 ```
 $ image_check
-Usage: image_check -u URL [-b MIN_BYTES_TO_ALERT_ON]
+Usage: image_check -u URL [-b MIN_BYTES_TO_ALERT_ON] [-j|-json]
 Ex: image_check -u http://www.google.com -b 1k
-
+```
+### Formatted Output ###
+```
 $ image_check -u http://www.google.com -b 1k
 Image files > 1.00 kB (1000 bytes)
-    1.83 kB http://www.google.com/images/icons/product/chrome-48.png
-  209.03 kB http://www.google.com/logos/doodles/2014/halloween-2014-5647569745084416.3-hp.gif
+    8.23 kB https://www.google.com/images/srpr/logo9w.png
+    1.83 kB https://www.google.com/images/icons/product/chrome-48.png
+```
+### JSON output
+```
+$ image_check -u https://www.google.com -b 1k -j
+{ url: 'https://www.google.com',
+  byte_threshold: 1000,
+  images:
+   [ { image_url: 'https://www.google.com/images/srpr/logo9w.png',
+       bytes: 8228 },
+     { image_url: 'https://www.google.com/images/icons/product/chrome-48.png',
+       bytes: 1834 } ] }
 ```
--- image_check.js
+++ image_check.js
@@ -3,21 +3,24 @@
 // Get image sizes for images on a given url
 var Scraper = require('image-scraper'),
     request = require('request'),
+    async = require('async'),
     Filesize = require('filesize'),
     colors = require('colors/safe'),
     sprintf=require("sprintf-js").sprintf,
     argv = require('minimist')(process.argv.slice(2));
 
-var usage = "Usage: image_check -u URL [-b MIN_BYTES_TO_ALERT_ON]\n" +
+var usage = "Usage: image_check -u URL [-b MIN_BYTES_TO_ALERT_ON] [-j|-json]\n" +
             "Ex: " + colors.grey("image_check -u http://www.google.com -b 1k");
+
 
 if (!argv.u) {
     console.log(usage);
     process.exit(1);
 }
 
-var test_url = argv.u,
-    max_size_bytes = argv.b || 0;
+var url = argv.u,
+    max_size_bytes = argv.b || 0,
+    json_output = argv.j || argv.json || false;
 
 if (typeof(max_size_bytes) === "string" && max_size_bytes.match(/k/i)){
     max_size_bytes = max_size_bytes.replace(/k/i, "");
@@ -30,25 +33,63 @@ if (isNaN(max_size_bytes)){
     process.exit(1);
 }
 
-if (!test_url.match(/http/i) && !test_url.match(/https/i)) {
-    test_url = "http://" + test_url;
+if (!url.match(/http/i) && !url.match(/https/i)) {
+    url = "http://" + url;
 }
 
-if (max_size_bytes) {
-    console.log(colors.bold("Image files >" + Filesize(max_size_bytes) + " (" + max_size_bytes + " bytes)"));
-}
+var scraper = new Scraper (url);
 
-var scraper = new Scraper (test_url);
-
+var asyncTasks = [];
 scraper.on("image", function(image){
-    processImage(image);
+    asyncTasks.push(function(callback) {
+        processImage(image, callback);
+    });
 });
 
 scraper.on("end", function(){
-    console.log("END");
+    async.parallel(asyncTasks, function(){
+        // Sort the output by bytes descending
+        json.images.sort(function(a, b){
+            return (b.bytes - a.bytes);
+        });
+
+        if (json_output) {
+            console.log(json);
+        } else {
+            if (max_size_bytes) {
+                console.log(colors.bold("Image files >" + Filesize(max_size_bytes) + " (" + max_size_bytes + " bytes)"));
+            }
+
+            if (json.images.length > 0) {
+                json.images.forEach(function(image_data){
+                    var image_url = image_data.image_url,
+                        file_size_bytes = image_data.bytes,
+                        file_size = Filesize(file_size_bytes),
+                        formatted_file_size = sprintf("%11s", file_size);
+
+                    // Images 3x the max size get highlighted in red
+                    var formatted_output = colors.yellow(formatted_file_size);
+                    if (file_size_bytes > (3 * max_size_bytes)) {
+                        formatted_output = colors.red(formatted_file_size);
+                    }
+                    formatted_output += " " + colors.cyan(image_url);
+
+                    console.log(formatted_output);
+                });
+            } else {
+                console.log("No images found.");
+            }
+        }
+    });
 });
 
-function processImage(image) {
+var formatted_output_arr = {},
+    json = {
+        url             : url,
+        byte_threshold  : max_size_bytes,
+        images          : []
+    };
+function processImage(image, callback) {
     var image_url = image.address;
     request.head(image_url, function(err, res){
         if (err) {
@@ -56,20 +97,16 @@ function processImage(image) {
         }
 
         if (res && res.headers['content-length']){
-            var file_size_bytes = +(res.headers['content-length']),
-                file_size = Filesize(file_size_bytes);
+            var file_size_bytes = +(res.headers['content-length']);
 
             if (file_size_bytes > max_size_bytes) {
-                var formatted_file_size = sprintf("%11s", file_size);
-                // Images 3x the max size get highlighted in red
-                var formatted_output = colors.yellow(formatted_file_size);
-                if (file_size_bytes > (3 * max_size_bytes)) {
-                    formatted_output = colors.red(formatted_file_size);
-                }
-                formatted_output += " " + colors.cyan(image_url);
-                console.log(formatted_output);
+                json.images.push({
+                    image_url: image_url,
+                    bytes: file_size_bytes
+                });
             }
         }
+        callback();
     });
 }
 scraper.scrape();
