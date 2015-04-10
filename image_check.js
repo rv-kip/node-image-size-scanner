821d2517961def81ab95227d017e475c0cdae9cb
Kip Gebhardt <kgebhardt23@gmail.com>
2015-04-09 19:23:22 -0700

    Finalized move to

--- image_check.js
+++ image_check.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-
 // Get image sizes for images on a given url
 var request         = require('request'),
     async           = require('async'),
-    $               = require('cheerio'),
+    cheerio         = require('cheerio'),
     Filesize        = require('filesize'),
     colors          = require('colors/safe'),
     sprintf         = require("sprintf-js").sprintf,
@@ -12,7 +11,6 @@ var request         = require('request'),
 var usage = "Usage: image_check -u URL [-b MIN_BYTES_TO_ALERT_ON] [-j|-json]\n" +
             "Ex: " + colors.grey("image_check -u http://www.google.com -b 1k");
 
-
 if (!argv.u) {
     console.log(usage);
     process.exit(1);
@@ -20,7 +18,13 @@ if (!argv.u) {
 
 var url = argv.u,
     max_size_bytes = argv.b || 0,
-    json_output = argv.j || argv.json || false;
+    json_output = argv.j || argv.json || false,
+    formatted_output_arr = {},
+    json = {
+        url             : url,
+        byte_threshold  : max_size_bytes,
+        images          : []
+    };
 
 if (typeof(max_size_bytes) === "string" && max_size_bytes.match(/k/i)){
     max_size_bytes = max_size_bytes.replace(/k/i, "");
@@ -37,82 +41,69 @@ if (!url.match(/http/i) && !url.match(/https/i)) {
     url = "http://" + url;
 }
 
-var asyncTasks = [];
-
 request(url, function (err, response, body){
     if (err) {
         console.error(err);
         return;
     }
 
-    var parsedHTML = $.load(body);
-    // console.log(parsedHTML);
+    var $ = cheerio.load(body),
+        images = $('img').toArray(),
+        asyncTasks = [];
 
-    var imageURLs = [];
-    parsedHTML('img').map(function(i, link) {
-        var href = $(link).attr('src');
 
-        if (!href.match(/\/\//)){
-            href = url + href;
+    // Process each image
+    images.forEach(function(image){
+        var image_url = image.attribs.src;
+        if (/^\/\//.test(image_url)) {
+            image_url = 'http:' + image_url;
         }
-
-        imageURLs.push(href);
+        if (!/^https?:\/\//.test(image_url)) {
+            image_url = url + image_url;
+        }
+        asyncTasks.push(function(callback) {
+            processImage(image_url, callback);
+        });
     });
 
-    console.log(imageURLs);
+    // Done
+    async.parallel(asyncTasks, function(){
+        // Sort the output by bytes descending
+        json.images.sort(function(a, b){
+            return (b.bytes - a.bytes);
+        });
 
+        if (json_output) {
+            console.log(json);
+        } else {
+            if (max_size_bytes) {
+                console.log(colors.bold("Image files >" + Filesize(max_size_bytes) + " (" + max_size_bytes + " bytes)"));
+            }
+
+            if (json.images.length > 0) {
+                json.images.forEach(function(image_data){
+                    var image_url = image_data.image_url,
+                        file_size_bytes = image_data.bytes,
+                        file_size = Filesize(file_size_bytes),
+                        formatted_file_size = sprintf("%11s", file_size);
+
+                    // Images 3x the max size get highlighted in red
+                    var formatted_output = colors.yellow(formatted_file_size);
+                    if (file_size_bytes > (3 * max_size_bytes)) {
+                        formatted_output = colors.red(formatted_file_size);
+                    }
+                    formatted_output += " " + colors.cyan(image_url);
+
+                    console.log(formatted_output);
+                });
+            } else {
+                console.log("No images found at " + url);
+            }
+        }
+    });
 });
-// scraper.on("image", function(image){
-//     asyncTasks.push(function(callback) {
-//         processImage(image, callback);
-//     });
-// });
 
-// scraper.on("end", function(){
-//     async.parallel(asyncTasks, function(){
-//         // Sort the output by bytes descending
-//         json.images.sort(function(a, b){
-//             return (b.bytes - a.bytes);
-//         });
-
-//         if (json_output) {
-//             console.log(json);
-//         } else {
-//             if (max_size_bytes) {
-//                 console.log(colors.bold("Image files >" + Filesize(max_size_bytes) + " (" + max_size_bytes + " bytes)"));
-//             }
-
-//             if (json.images.length > 0) {
-//                 json.images.forEach(function(image_data){
-//                     var image_url = image_data.image_url,
-//                         file_size_bytes = image_data.bytes,
-//                         file_size = Filesize(file_size_bytes),
-//                         formatted_file_size = sprintf("%11s", file_size);
-
-//                     // Images 3x the max size get highlighted in red
-//                     var formatted_output = colors.yellow(formatted_file_size);
-//                     if (file_size_bytes > (3 * max_size_bytes)) {
-//                         formatted_output = colors.red(formatted_file_size);
-//                     }
-//                     formatted_output += " " + colors.cyan(image_url);
-
-//                     console.log(formatted_output);
-//                 });
-//             } else {
-//                 console.log("No images found.");
-//             }
-//         }
-//     });
-// });
-
-var formatted_output_arr = {},
-    json = {
-        url             : url,
-        byte_threshold  : max_size_bytes,
-        images          : []
-    };
-function processImage(image, callback) {
-    var image_url = image.address;
+function processImage(image_url, callback) {
     request.head(image_url, function(err, res){
         if (err) {
             return console.error(colors.red("Error"), err);
@@ -120,15 +111,14 @@ function processImage(image, callback) {
 
         if (res && res.headers['content-length']){
             var file_size_bytes = +(res.headers['content-length']);
-
             if (file_size_bytes > max_size_bytes) {
-                json.images.push({
+                var data = {
                     image_url: image_url,
                     bytes: file_size_bytes
-                });
+                };
+                json.images.push(data);
             }
         }
         callback();
     });
 }
-// scraper.scrape();
